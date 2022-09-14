957c7e1d8250c7e4ab02c132f102410671637bfa
zolii <zoltanvagany058@gmail.com>
2022-09-14 21:07:11 +0200

    Initial commit

---
+++ app/src/main/java/com/example/beadando/MainActivity.java
@@ -0,0 +1,106 @@
+package com.example.beadando;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button Bt;
+    private String st;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Bt = findViewById(R.id.kurva);
+        EditText username = findViewById(R.id.textBox);
+        EditText pass = findViewById(R.id.passBox);
+        Bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), username.getText() + "\n" + pass.getText(), Toast.LENGTH_LONG).show();
+                //Itt akad meg!
+                HashMap<String, String> params = null;
+
+                params.put("username", username.getText().toString());
+                params.put("password", pass.getText().toString());
+                StringBuilder sbParams = new StringBuilder();
+                int i = 0;
+                for (String key : params.keySet()) {
+                    try {
+                        if (i != 0){
+                            sbParams.append("&");
+                        }
+                        sbParams.append(key).append("=")
+                                .append(URLEncoder.encode(params.get(key), "UTF-8"));
+
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    }
+                    i++;
+                }
+                HttpURLConnection conn = null;
+                try{
+                    String url = "https://oldal.vaganyzoltan.hu/API/login.php";
+                    URL urlObj = new URL(url);
+                    conn = (HttpURLConnection) urlObj.openConnection();
+                    conn.setDoOutput(true);
+                    conn.setRequestMethod("POST");
+                    conn.setRequestProperty("Accept-Charset", "UTF-8");
+
+                    conn.setReadTimeout(10000);
+                    conn.setConnectTimeout(15000);
+
+                    conn.connect();
+
+                    String paramsString = sbParams.toString();
+
+                    DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
+                    wr.writeBytes(paramsString);
+                    wr.flush();
+                    wr.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    InputStream in = new BufferedInputStream(conn.getInputStream());
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                    StringBuilder result = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        result.append(line);
+                    }
+
+                    Log.d("test", "result from server: " + result.toString());
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (conn != null) {
+                        conn.disconnect();
+                    }
+                }
+            }
+        });
+    }
+}
