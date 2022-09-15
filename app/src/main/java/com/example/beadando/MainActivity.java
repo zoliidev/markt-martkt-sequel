1c30ab10931cdd8a753620a071cfb645bcd3845f
Zolii_ <zolii@hugaming.hu>
2022-09-15 18:39:28 +0200

    Nyelvezet, átláthatóság javítva

--- app/src/main/java/com/example/beadando/MainActivity.java
+++ app/src/main/java/com/example/beadando/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.beadando;
 
+import static com.example.beadando.kollekcio.WebRequest.sendWebRequest;
+
 import android.os.Bundle;
-import android.os.StrictMode;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,105 +10,31 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import org.json.JSONObject;
-
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
 import java.util.HashMap;
-
 
 public class MainActivity extends AppCompatActivity {
 
     private Button Bt;
     private String st;
-    private HashMap<String, String> params;
+
     //private JSONObject jsonObject;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Bt = findViewById(R.id.kurva);
+        Bt = findViewById(R.id.loginButton);
         EditText username = findViewById(R.id.textBox);
         EditText pass = findViewById(R.id.passBox);
         Bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Toast.makeText(getApplicationContext(), username.getText() + "\n" + pass.getText(), Toast.LENGTH_LONG).show();
-                params = new HashMap<>();
+                HashMap<String,String> params = new HashMap<>();
                 params.put("name", username.getText().toString());
                 params.put("password", pass.getText().toString());
 
-                StringBuilder sbParams = new StringBuilder();
-                int i = 2;
-                for (String key : params.keySet()) {
-                    try {
-                        if (i != 0){
-                            sbParams.append("&");
-                        }
-                        sbParams.append(key).append("=")
-                                .append(URLEncoder.encode(params.get(key), "UTF-8"));
-
-                    } catch (UnsupportedEncodingException e) {
-                        e.printStackTrace();
-                    }
-                    i++;
-                }
-                HttpURLConnection conn = null;
-                StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
-                        .permitAll().build();
-                StrictMode.setThreadPolicy(policy);
-                try{
-                    String url = "https://oldal.vaganyzoltan.hu/api/login.php";
-                    URL urlObj = new URL(url);
-                    conn = (HttpURLConnection) urlObj.openConnection();
-                    conn.setDoOutput(true);
-                    conn.setRequestMethod("POST");
-                    conn.setRequestProperty("Accept-Charset", "UTF-8");
-
-                    conn.setReadTimeout(10000);
-                    conn.setConnectTimeout(15000);
-
-                    conn.connect();
-
-                    String paramsString = sbParams.toString();
-
-                    DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
-                    wr.writeBytes(paramsString);
-                    wr.flush();
-                    wr.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-                try {
-                    InputStream in = new BufferedInputStream(conn.getInputStream());
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-                    StringBuilder result = new StringBuilder();
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        result.append(line);
-                    }
-
-                    Log.d("test", "result from server: " + result.toString());
-                    Toast.makeText(getApplicationContext(), result.toString(), Toast.LENGTH_LONG).show();
-                    //jsonObject = new JSONObject();
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } finally {
-                    if (conn != null) {
-                        conn.disconnect();
-                    }
-                }
+                String result = sendWebRequest("https://oldal.vaganyzoltan.hu/api/login.php", params);
+                Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
             }
         });
     }
---
+++ app/src/main/java/com/example/beadando/kollekcio/WebRequest.java
@@ -0,0 +1,85 @@
+package com.example.beadando.kollekcio;
+
+import android.os.StrictMode;
+import android.util.Log;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+
+public class WebRequest {
+    public static String sendWebRequest(String url, HashMap<String, String> params) {
+        //Toast.makeText(getApplicationContext(), username.getText() + "\n" + pass.getText(), Toast.LENGTH_LONG).show();
+        StringBuilder sbParams = new StringBuilder();
+        int i = 2;
+        for (String key : params.keySet()) {
+            try {
+                if (i != 0){
+                    sbParams.append("&");
+                }
+                sbParams.append(key).append("=")
+                        .append(URLEncoder.encode(params.get(key), "UTF-8"));
+
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            i++;
+        }
+        HttpURLConnection conn = null;
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        try{
+            URL urlObj = new URL(url);
+            conn = (HttpURLConnection) urlObj.openConnection();
+            conn.setDoOutput(true);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Accept-Charset", "UTF-8");
+
+            conn.setReadTimeout(10000);
+            conn.setConnectTimeout(15000);
+
+            conn.connect();
+
+            String paramsString = sbParams.toString();
+
+            DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
+            wr.writeBytes(paramsString);
+            wr.flush();
+            wr.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            InputStream in = new BufferedInputStream(conn.getInputStream());
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            StringBuilder result = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                result.append(line);
+            }
+
+            Log.d("test", "result from server: " + result);
+            //Toast.makeText(getApplicationContext(), result.toString(), Toast.LENGTH_LONG).show();
+            //jsonObject = new JSONObject();
+            return result.toString();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (conn != null) {
+                conn.disconnect();
+            }
+        }
+        return null;
+    }
+}
