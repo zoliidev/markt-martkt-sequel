8343a8672ec080bbebf9c6701c1c2d4721d2d15a
Zolii_ <zolii@hugaming.hu>
2022-09-18 13:01:13 +0200

    Kép tesztelés, scroll view átalakítás (a kép betöltése nem működik még!)

--- app/src/main/java/com/example/beadando/MainActivity.java
+++ app/src/main/java/com/example/beadando/MainActivity.java
@@ -3,14 +3,13 @@ package com.example.beadando;
 import static com.example.beadando.kollekcio.WebRequest.sendWebRequest;
 
 import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
--- app/src/main/java/com/example/beadando/ScrollingActivity.java
+++ app/src/main/java/com/example/beadando/ScrollingActivity.java
@@ -1,5 +1,8 @@
 package com.example.beadando;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import com.google.android.material.appbar.CollapsingToolbarLayout;
@@ -9,13 +12,21 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.os.StrictMode;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.example.beadando.databinding.ActivityScrollingBinding;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class ScrollingActivity extends AppCompatActivity {
 
     private ActivityScrollingBinding binding;
+    private ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +38,16 @@ public class ScrollingActivity extends AppCompatActivity {
         Toolbar toolbar = binding.toolbar;
         setSupportActionBar(toolbar);
         CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
-        toolBarLayout.setTitle(getTitle());
+        toolBarLayout.setTitle("Apple Iphone 13");
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        try {
+            ImageView i = (ImageView)findViewById(R.id.tesztkep);
+            Bitmap bitmap = BitmapFactory.decodeStream((InputStream)new URL("https://oldal.vaganyzoltan.hu/prod-img/iphone.jpg").getContent());
+            i.setImageBitmap(bitmap);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
